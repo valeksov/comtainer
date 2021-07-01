c380bb9dc75476dc53be60437fee0b197c91b94e
VaskoTsv <vask_o@abv.bg>
2021-07-01 11:51:26 +0300

    Move xlsx logic to own component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,39 +1,11 @@
-import { Button, Input } from '@material-ui/core';
-import React, { useCallback, useState } from 'react';
-import { GenerateJSONFromXls } from 'services/generate-json';
-import XLSX from 'xlsx';
-import styles from './styles/App.module.scss';
+import { XlsxConverter } from 'components/main/XlsxConverter';
+import React from 'react';
+import styles from './App.module.scss';
 
 function App() {
-    const [selectedFile, setSelectedFile] = useState(null);
-
-    const handleFileUpload = e => {
-        setSelectedFile(e.target.files[0]);
-    };
-
-    const handleFileConversion = useCallback(() => {
-        if (!selectedFile) {
-            return;
-        }
-
-        const fileReader = new FileReader();
-        fileReader.readAsBinaryString(selectedFile);
-
-        fileReader.onload = async event => {
-            const fileData = event.target.result;
-            const finalJSON = GenerateJSONFromXls.generateFinalJSON(XLSX.read(fileData, { type: 'binary' }));
-            console.log({ finalJSON });
-            // TODO vasko - uncomment if we need the functionality to download the json data as a .json file.
-            // exportToJson(finalJSON);
-        };
-    }, [selectedFile]);
-
     return (
         <div className={styles.container}>
-            <Input type="file" color="primary" onChange={handleFileUpload} />
-            <Button variant="contained" color="primary" onClick={handleFileConversion}>
-                Convert
-            </Button>
+            <XlsxConverter />
         </div>
     );
 }
---
+++ src/components/main/XlsxConverter/XlsxConverter.tsx
@@ -0,0 +1,42 @@
+import { Button, Input } from '@material-ui/core';
+import React, { useCallback, useState } from 'react';
+import { GenerateJSONFromXls } from 'services/generate-json';
+import XLSX from 'xlsx';
+import styles from './XlsxConverter.module.scss';
+
+const XlsxConverterComponent = () => {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileUpload = e => {
+        setSelectedFile(e.target.files[0]);
+    };
+
+    const handleFileConversion = useCallback(() => {
+        if (!selectedFile) {
+            return;
+        }
+
+        const fileReader = new FileReader();
+        fileReader.readAsBinaryString(selectedFile);
+
+        fileReader.onload = async event => {
+            const fileData = event.target.result;
+            const finalJSON = GenerateJSONFromXls.generateFinalJSON(XLSX.read(fileData, { type: 'binary' }));
+            console.log({ finalJSON });
+            // TODO vasko - uncomment if we need the functionality to download the json data as a .json file.
+            // exportToJson(finalJSON);
+        };
+    }, [selectedFile]);
+
+    return (
+        <div className={styles.container}>
+            <Input type="file" color="primary" onChange={handleFileUpload} />
+            <Button variant="contained" color="primary" onClick={handleFileConversion}>
+                Convert
+            </Button>
+        </div>
+    );
+};
+
+const XlsxConverter = XlsxConverterComponent;
+export { XlsxConverter };
